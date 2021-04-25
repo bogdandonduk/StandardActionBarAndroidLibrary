cac514230b0885618b025bce3dd21b24a06a8b17
bogdandonduk <bogdandonduk@gmail.com>
2021-04-25 17:12:54 +0700

    Commit 12

---
+++ StandardActionBarAndroid/src/main/java/bogdandonduk/androidlibs/standardactionbarandroid/OptionsMenuItemsKeysExtensionVocabulary.kt
@@ -0,0 +1,7 @@
+package bogdandonduk.androidlibs.standardactionbarandroid
+
+object OptionsMenuItemsKeysExtensionVocabulary {
+    const val KEY_ITEM_SEARCH = "search"
+    const val KEY_ITEM_SETTINGS = "settings"
+    const val KEY_ITEM_OVERFLOW_MENU = "overflow_menu"
+}
